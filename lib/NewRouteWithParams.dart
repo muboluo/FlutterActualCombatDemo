f28c88cc50a64b37febd7c388979ccdf67305e9e
zhangheng24 <zhangheng24@58.com>
2021-05-13 19:36:38 +0800

    imp,assets,add image assets 2 project

--- lib/NewRouteWithParams.dart
+++ lib/NewRouteWithParams.dart
@@ -1,4 +1,3 @@
-
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
@@ -9,25 +8,24 @@ class NewRouteWithParams extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final wordPail = WordPair.random().toString();
     return Scaffold(
       appBar: AppBar(
-        // title: Text("带传参的页面"),
+        title: Text("带传参的页面"),
       ),
       body: Center(
         child: Column(
           children: <Widget>[
             Text(text),
             RaisedButton(
-              onPressed: () =>
-                  Navigator.of(context).pop("洞洞收到"),
+              onPressed: () => Navigator.of(context).pop("洞洞收到"),
               child: Text("带参数返回"),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text("english_word包随机方法测试 $wordPail"),
-            )
+            ),
+            Image.asset('images/ic_launcher.png'),
           ],
         ),
       ),
