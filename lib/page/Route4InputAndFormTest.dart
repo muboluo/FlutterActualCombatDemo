cdbb7a1c337bdd8cb395008aca968312451b5858
zhangheng24 <zhangheng24@58.com>
2021-05-28 11:46:07 +0800

    imp,Route4InputAndFormText,添加 TextField 的 controller

--- lib/page/Route4InputAndFormTest.dart
+++ lib/page/Route4InputAndFormTest.dart
@@ -11,6 +11,22 @@ class Route4InputAndFormTest extends StatefulWidget {
 }
 
 class _Route4InputAndFormState extends State<Route4InputAndFormTest> {
+  TextEditingController _nameController = TextEditingController();
+  FocusNode node1 = FocusNode();
+  FocusNode node2 = FocusNode();
+  FocusScopeNode scopeNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController.addListener(() {
+      print(_nameController.text);
+    });
+    _nameController.text = "HELLO TEST";
+    _nameController.selection =
+        TextSelection(baseOffset: 2, extentOffset: _nameController.text.length);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +36,8 @@ class _Route4InputAndFormState extends State<Route4InputAndFormTest> {
       body: Column(
         children: <Widget>[
           TextField(
+            focusNode: node1,
+            controller: _nameController,
             autofocus: true,
             decoration: InputDecoration(
                 labelText: "用户名",
@@ -27,12 +45,29 @@ class _Route4InputAndFormState extends State<Route4InputAndFormTest> {
                 prefixIcon: Icon(Icons.person)),
           ),
           TextField(
+            focusNode: node2,
             decoration: InputDecoration(
                 labelText: "密码",
                 hintText: "您的登录密码",
                 prefixIcon: Icon(Icons.lock)),
             obscureText: true,
           ),
+          ElevatedButton(
+            onPressed: () {
+              if (null == scopeNode) {
+                scopeNode = FocusScope.of(context);
+              }
+              scopeNode.requestFocus(node2);
+            },
+            child: Text('切换焦点'),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                node1.unfocus();
+                node2.unfocus();
+              },
+              // 所有输入框都失去焦点之后，键盘会自动隐藏。
+              child: Text('所有输入框都失去焦点，隐藏键盘')),
         ],
       ),
     );
